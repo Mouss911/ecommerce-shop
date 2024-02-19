3fbcba6979f6f942600e8074c193b6f4292e79e1
MoUsSa <diattamoussa010@gmail.com>
2024-02-19 00:16:23 +0000

    panier

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import { IoMdClose } from 'react-icons/io';
 
-const CartItem = () => {
-  return <div>CartItem</div>;
+const CartItem = ({ item }) => {
+  const { id, title, image, price, amount } = item;
+  return (
+    <div className='flex'>
+      <div className='w-full min-h-[150px] flex items-center gap-x-4'>
+        {/* image */}
+        <Link to={`/product/${id}`}>
+          <img className='max-w-[80px]' src={image} alt='voir' />
+        </Link>
+        <div className='w-full flex flex-col'>
+          {/* title & remove icon */}
+          <div className='flex justify-between mb-2'>
+            {/* title */}
+            <Link to={`/product/${id}`} className='text-sm uppercasefont-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+            {/* remove icon */}
+            <div className='text-xl cursor-pointer'>
+              <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
+            </div>
+          </div>
+          
+        </div>
+      </div>
+    </div>
+  )
 };
 
 export default CartItem;
