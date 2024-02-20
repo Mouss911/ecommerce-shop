d2a8c72d1c8bfb09cbf24c6c888478977f7887a1
MoUsSa <diattamoussa010@gmail.com>
2024-02-20 23:44:06 +0000

    quelques ajouts

--- src/components/CartItem.js
+++ src/components/CartItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import { IoMdClose } from 'react-icons/io';
+import { IoMdAdd, IoMdClose, IoMdRemove } from 'react-icons/io';
 
 const CartItem = ({ item }) => {
   const { id, title, image, price, amount } = item;
   return (
-    <div className='flex'>
+    <div className='flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500'>
       <div className='w-full min-h-[150px] flex items-center gap-x-4'>
         {/* image */}
         <Link to={`/product/${id}`}>
@@ -15,13 +15,31 @@ const CartItem = ({ item }) => {
           {/* title & remove icon */}
           <div className='flex justify-between mb-2'>
             {/* title */}
-            <Link to={`/product/${id}`} className='text-sm uppercasefont-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
+            <Link to={`/product/${id}`} className='text-sm uppercase font-medium max-w-[240px] text-primary hover:underline'>{title}</Link>
             {/* remove icon */}
             <div className='text-xl cursor-pointer'>
               <IoMdClose className='text-gray-500 hover:text-red-500 transition' />
             </div>
           </div>
-          
+          <div className='flex gap-x-2 h-[36px] text-sm'>
+            {/* quantity */}
+            <div className='flex flex-1 max-w-[100px] items-center h-full border text-primary font-medium'>
+              {/* minus icon */}
+              <div className='flex-1 flex justify-center items-center cursor-pointer'>
+                <IoMdRemove />
+              </div>
+              {/* amount */}
+              <div className='h-full flex justify-center items-center px-2'>{amount}</div>
+              {/* plus icon */}
+              <div className='flex-1 h-full flex justify-center items-center cursor-pointer'>
+                <IoMdAdd />
+              </div>
+            </div>
+            {/* item price */}
+            <div className='flex-1 flex justify-center items-center'>$ {price}</div>
+            {/* final price */}
+            <div className='flex-1 flex justify-center items-center cursor-pointer text-primary font-medium'>{`$ ${price * amount}`}</div>
+          </div>
         </div>
       </div>
     </div>
